e29a0b83430781cc703f40dcd5f2a78a51e9a1ef
parcpaes <spaesmx@gmail.com>
2021-11-04 09:37:46 -0400

    Solving a Duck problem with different behaviors using Strategy Pattern

---
+++ src/main/java/strategy/duck/Duck.java
@@ -0,0 +1,30 @@
+package strategy.duck;
+
+public abstract class Duck  {
+    QuackBehavior quackBehavior;
+    FlyBehavior flyBehavior;
+    public Duck(){
+
+    }
+    public abstract void display();
+
+    public void performFly(){
+        flyBehavior.fly();
+    }
+
+    public void performQuack(){
+        quackBehavior.quack();
+    }
+
+    public void swim(){
+        System.out.println("All ducks float, even decoys!");
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
+
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+}
---
+++ src/main/java/strategy/duck/FlyWithWings.java
@@ -0,0 +1,9 @@
+package strategy.duck;
+
+public class FlyWithWings implements FlyBehavior{
+
+    @Override
+    public void fly() {
+        System.out.println("I'm flying");
+    }
+}
